bf292f1384848e6a1c813187f933336758e36ae0
HJBowers <HJBowers@gmail.com>
2017-06-08 09:34:55 -0700

    GET request is working, buttons are populated, but are not hidden.

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Mutably Starter Project
 For goal #383
+@HJBowers
 
 ## To get this project running
 1. `npm install`
 1. `npm start` to run (uses nodemon)
+
+
+## Specifications
+
+- [x] 5: Your repo is a fork of mutably-starter.
+- [x] 5: Your repo has a README with instructions for how to run your project.
+- [x] 15: Your app is SPA (single page app). All CRUD actions take place on the same page, preferably the root (/) route.
+- [ ] 10: All interaction with the API happens with jQuery’s AJAX function – don’t submit data via forms. You can use form html tags, but do all your form submission in your js. Make use of jQuery’s event.preventDefault().
+- [ ] 15: A user can read and display all the data for a resource.
+- [ ] 10: A user can create a new item via a create form. When the user creates a new item, that item should either get appended to the page or all the items should get re-retrieved in the js. No full page refresh.
+- [ ] 10: A user can update an existing item. Updating happens inline. This means that there is an edit button next to each item that, when clicked, the item text gets replaced with an pre-populated editable, input field. And the edit button becomes a save button. Once the save button is clicked and success message comes back from the server, then then input gets replaced with the updated text. No page refresh.
+- [ ] 10: A user can delete an existing item via a delete button next to each item. No page refresh.
+- [ ] 10: Use a UI library to make your site look nice.
+- [x] 5: The artifact produced is properly licensed, preferably with the MIT license.
+- [ ] 5: App is deployed on Heroku.
+
+### Stretch
+
+ Create another version of your front-end using a front-end framework such as React or Angular.
--- public/script.js
+++ public/script.js
@@ -1,7 +1,93 @@
-console.log("Sanity Check: JS is working!");
+console.log("Sanity Check: JS is working!")
 
 $(document).ready(function(){
 
-  // code in here
+  $("#getAllPokemon").on('click', function(data) {
+    getAllPokemon(data)
+  })
 
+//Create new pokemon
+  $(document).on('click', '.add-btn', function(data) {
+    let id = $(this).data('id')
+    $.ajax({
+      method: 'POST',
+      url: 'https://mutably.herokuapp.com/pokemon/',
+      success: HandlePokeNameAddRes
+    })
+  })
+
+
+
+//Delete a pokemon
+  $(document).on('click', '.delete-btn', function(data) {
+    let id = $(this).data('id')
+    $.ajax({
+      method: 'DELETE',
+      url: 'https://mutably.herokuapp.com/pokemon/' +id,
+      success: HandlePokeNameDeleteRes
+    })
+  })
+
+
+
+//Update a pokemon
+  $(document).on('click', '.save-btn', function(data) {
+    let id = $(this).data('id')
+    let updatedName = $('input-' + id + 'input').val()
+    $.ajax({
+      method: 'PUT',
+      url: 'https://mutably.herokuapp.com/pokemon/' +id,
+      data: {name: updatedName},
+      success: HandlePokeNameUpdateRes
+    })
+  })
+
+
+//Edit Button changes text to an input field AND changes edit button to save button
+  $(document).on('click', '.edit-btn', function(data) {
+      let id = $(this).data('id')
+      $('.name-' + id).hide()
+      $('.input-' + id).show()
+      $('.edit-' + id).hide()
+      $('.save-' +id).show()
+  })
 });
+
+
+//Get all pokemon
+function getAllPokemon() {
+  $('list-group').html('')
+  $.ajax({
+    method: 'GET',
+    url: 'https://mutably.herokuapp.com/pokemon'
+  }).done(function(results) {
+    console.log(results);
+    for (var i = 0; i < results.pokemon.length; i++) {
+      $('.list-group').append('<li class="list-group-item item-' + results.pokemon[i]._id + '">'
+      +'<button class="edit-btn edit-' + results.pokemon[i]._id + '" data-id="' + results.pokemon[i]._id + '">Edit</button>'
+      +'<button class="save-btn save-' + results.pokemon[i]._id + '" data-id="' + results.pokemon[i]._id + '">Save</button>'
+      +'<span class="name-' + results.pokemon[i]._id + '">&nbsp' + results.pokemon[i].name + '&nbsp</span>'
+      +'<span class="form-inline edit-form input-' + results.pokemon[i]._id + '">&nbsp<input class="form-control" value="' + results.pokemon[i].name + '"/></span>'
+      +'<button class="delete-btn">Delete</button>'
+      +'</li>')
+    }
+  })
+}
+
+function HandlePokeNameAddRes(data) {
+  console.log(data)
+  getAllPokemon()
+}
+
+function HandlePokeNameDeleteRes(data) {
+  $('.item-' + data._id).remove
+}
+
+function HandlePokeNameUpdateRes(data) {
+  let id = data._id
+  $('.name-' + id).html('&nbsp;'+data.name)
+  $('.name-' + id).show()
+  $('.input-' + id).hide()
+  $('.edit-' + id).show()
+  $('.save-' +id).hide()
+}
