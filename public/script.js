15e61b5a3de975cd15f55f17cc18d5faeeb579c4
HJBowers <HJBowers@gmail.com>
2017-06-19 15:43:27 -0700

    this commit message sucks, sorry

--- public/script.js
+++ public/script.js
@@ -2,23 +2,54 @@ console.log("Sanity Check: JS is working!")
 
 $(document).ready(function(){
 
+  //Loads all pokemon on page
   $("#getAllPokemon").on('click', function(data) {
     getAllPokemon(data)
   })
 
-//Create new pokemon
-  $(document).on('click', '.add-btn', function(data) {
-    let id = $(this).data('id')
+
+  //Open New Pokemon form
+  $(document).on('click', '.addNewPoke-btn', function(data) {
+    $('#addPokeForm').show()
+  })
+
+
+  //Create new pokemon
+  $('#addPokeForm').on('submit', function(event) {
+    event.preventDefault()
+
+    // let id = $(this).data('id')
+    // let url = "https://img.pokemondb.net/artwork/"
+    // let pokeName = $("#name").val().toLowerCase()
+    // $("#image").val(url + pokeName + ".jpg")
+
+    let newPokeData = $(this).serialize()
+    console.log("newPokeData:::", newPokeData)
+    $(this).trigger("reset")
     $.ajax({
       method: 'POST',
       url: 'https://mutably.herokuapp.com/pokemon/',
-      success: HandlePokeNameAddRes
+      data: newPokeData,
+      success: newPokeDataResponse
+    })
+  })
+
+
+  //Update a pokemon
+  $(document).on('click', '.save-btn', function() {
+    let id = $(this).data('id')
+    let updatedName = $('.input-' + id + 'input').val()
+    $.ajax({
+      method: 'PUT',
+      url: 'https://mutably.herokuapp.com/pokemon/' +id,
+      data: {name: updatedName},
+      success: HandlePokeNameUpdateRes
     })
   })
 
 
 
-//Delete a pokemon
+  //Delete a pokemon
   $(document).on('click', '.delete-btn', function(data) {
     let id = $(this).data('id')
     $.ajax({
@@ -30,28 +61,17 @@ $(document).ready(function(){
 
 
 
-//Update a pokemon
-  $(document).on('click', '.save-btn', function(data) {
-    let id = $(this).data('id')
-    let updatedName = $('input-' + id + 'input').val()
-    $.ajax({
-      method: 'PUT',
-      url: 'https://mutably.herokuapp.com/pokemon/' +id,
-      data: {name: updatedName},
-      success: HandlePokeNameUpdateRes
-    })
-  })
-
-
-//Edit Button changes text to an input field AND changes edit button to save button
+  //Edit Button changes text to an input field AND changes edit button to save button
   $(document).on('click', '.edit-btn', function(data) {
       let id = $(this).data('id')
-      $('.name-' + id).hide()
+      $('#name-' + id).hide()
+      // $('.pokedex-' + id).hide()
+      // $('.evolves_from-' + id).hide()
       $('.input-' + id).show()
       $('.edit-' + id).hide()
       $('.save-' +id).show()
   })
-});
+})
 
 
 //Get all pokemon
@@ -61,21 +81,34 @@ function getAllPokemon() {
     method: 'GET',
     url: 'https://mutably.herokuapp.com/pokemon'
   }).done(function(results) {
-    console.log(results);
+    console.log(results)
     for (var i = 0; i < results.pokemon.length; i++) {
-      $('.list-group').append('<li class="list-group-item item-' + results.pokemon[i]._id + '">'
-      +'<button class="edit-btn edit-' + results.pokemon[i]._id + '" data-id="' + results.pokemon[i]._id + '">Edit</button>'
-      +'<button class="save-btn save-' + results.pokemon[i]._id + '" data-id="' + results.pokemon[i]._id + '">Save</button>'
-      +'<span class="name-' + results.pokemon[i]._id + '">&nbsp' + results.pokemon[i].name + '&nbsp</span>'
-      +'<span class="form-inline edit-form input-' + results.pokemon[i]._id + '">&nbsp<input class="form-control" value="' + results.pokemon[i].name + '"/></span>'
-      +'<button class="delete-btn">Delete</button>'
+      $('.list-group').append('<li class="list-group-item item-' + results.pokemon[i]._id + '" >'
+      +'<img src=' + results.pokemon[i].image + '>'
+      +'<p></p>'
+      +'<button class="edit-btn edit-' + results.pokemon[i]._id + '" data-id="' + results.pokemon[i]._id + '" >Edit Name</button>'
+      +'<p></p>'
+      +'<span class="name-' + results.pokemon[i]._id + '" >' + "<strong>Pokémon Name: </strong>" + '<text id="name-' + results.pokemon[i]._id + '" >' + results.pokemon[i].name + '</text>' + '</span>'
+      +'<span class="form-inline edit-form input-' + results.pokemon[i]._id + '" hidden>&nbsp<input class="form-control" value="' + results.pokemon[i].name + '"/></span>'
+      +'<br></br>'
+      +'<span class="pokedex-' + results.pokemon[i]._id + '" >' + "<strong>Pokédex Number: </strong>" + results.pokemon[i].pokedex +'</span>'
+      // +'<span class="form-inline edit-form input-' + results.pokemon[i]._id + '" hidden>&nbsp<input class="form-control" value="' + results.pokemon[i].pokedex + '"/></span>'
+      +'<br></br>'
+      +'<span class="evolves_from-' + results.pokemon[i]._id + '" >' + "<strong>Evolves From: </strong>" + results.pokemon[i].evolves_from +'</span>'
+      // +'<span class="form-inline edit-form input-' + results.pokemon[i]._id + '" hidden>&nbsp<input class="form-control" value="' + results.pokemon[i].evolves_from + '"/></span>'
+
+      +'<br></br>'
+      +'<button class="save-btn save-' + results.pokemon[i]._id + '" data-id="' + results.pokemon[i]._id + '" hidden>Save</button>'
+      +'<p></p>'
+      +'<button class="delete-btn" >Delete</button>'
       +'</li>')
     }
   })
 }
 
-function HandlePokeNameAddRes(data) {
+function newPokeDataResponse(data) {
   console.log(data)
+  $('#addPokeForm').hide()
   getAllPokemon()
 }
 
@@ -85,8 +118,13 @@ function HandlePokeNameDeleteRes(data) {
 
 function HandlePokeNameUpdateRes(data) {
   let id = data._id
-  $('.name-' + id).html('&nbsp;'+data.name)
-  $('.name-' + id).show()
+  // $('#name-' + id).html('&nbsp;'+data.name)
+  // $('.pokedex-' + id).html('&nbsp;'+data.pokedex)
+  // $('.evolves_from-' + id).html('&nbsp;'+data.evolves_from)
+
+  $('#name-' + id).show()
+  // $('.pokedex-' + id).show()
+  // $('.evolves_from-' + id).show()
   $('.input-' + id).hide()
   $('.edit-' + id).show()
   $('.save-' +id).hide()
