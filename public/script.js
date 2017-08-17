6c836bcc30aa4fc5ae7544cd28a33ed1463780fb
HJBowers <HJBowers@gmail.com>
2017-08-16 17:26:12 -0700

    Added GET and POST with ajax, created form for adding books, created buttons for book list

--- public/script.js
+++ public/script.js
@@ -2,7 +2,45 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
-  
+  $('.show-books').on('click', function() {
+    event.preventDefault()
+    getAllBooks()
+  })
 
+  // GET for show all books
+  function getAllBooks() {
+    // $('ul .list-group-book').replaceAll('.list-group-book')
+    $.get("https://mutably.herokuapp.com/books", function(data, status) {
+      console.log(data);
+      var books = data.books
+      for (var i = 0; i < books.length; i++) {
+        $('.list-group').append(
+          '<ul class="list-group-book book-'+books[i]._id+'">'
+          +'<button class="btn btn-primary btn-edit-' + books[i]._id + '">Edit</button>'
+          +'<span class="book-'+books[i]._id+'">&nbsp' + books[i].title + '&nbsp</span>'
+          +'<span class="form-inline edit-form input-'+data.books[i]._id+'">&nbsp;<input class="form-control" value="'+data.books[i].title+'"/></span>'
+          +'<button class="btn btn-success pull-right btn-save-' + books[i]._id + '">Save</button>'
+          +'<button class="btn btn-danger pull-right btn-delete-' + books[i]._id + '">Delete</button>'
+          +'</ul>'
+        )
+      }
+    })
+  }
+
+
+
+  // Form POST
+  $('form').on('submit', function(event) {
+    event.preventDefault()
+    var title = $('.add-title').serialize();
+    var author = $('.add-author').serialize();
+    $.post("https://mutably.herokuapp.com/books",
+    {
+      title: title,
+      author: author
+    },
+    function(data, status) {
+      console.log("Book added!\nData Submitted::: ", data, "\nStatus::: ", status);
+    })
+  })
 });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
 app.use(express.static('public'))
 
 // set 'html' as the engine, using ejs's renderFile function
